870634244a8fc34ad43da96c450371a3d1de5e74
David Schonfeld <davidaschonfeld@gmail.com>
2026-03-04 17:59:06 -0500

    Added a Login Page, which worksNote: So far, the login page is only accessible via mainUrl/login, not the navigation bar yet. Yes, it even saves the token and userName into localStorage.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css'
 import {Routes, Route} from 'react-router-dom'
 import About from './pages/About'
 import Navbar from './components/Navbar'
+import LoginPage from './pages/LoginPage'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path = "/" element = {<div>Home Page</div>} />
         <Route path = "/about" element = {<About />} />
+        <Route path = "/login" element = {<LoginPage />} />
       </Routes>
     </>
   )
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,10 +6,18 @@ import App from './App.tsx'
 // My Additional Imports
 import { BrowserRouter} from 'react-router-dom'
 
+// Import from My Files
+import { AuthProvider } from './context/AuthContext.tsx'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
+  // <StrictMode>: Development-Only tool, deliberately runs every component twice to help find side effects/bugs that only occur on re-renders. Also warns about deprecated React features and unsafe patterns.
+  // <BrowserRouter>: Enables Routing between Different URLs
+  // <AuthProvider>: Wraps <App /> so every component in my app can access the AuthContext (which stores the token, userName, login function, logout function).
 )
