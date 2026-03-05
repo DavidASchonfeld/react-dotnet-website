37a7bf63ef1283ebd712e7867bb5cea367ea4e5b
David Schonfeld <davidaschonfeld@gmail.com>
2026-03-05 13:31:17 -0500

    Added basic MyLists page (nothing to display yet) which is a ProtectedRoute, so ProtectedRoute works.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css'
 
 // My Additional Imports
+//// React Router (External Library)
 import {Routes, Route} from 'react-router-dom'
+
+// My Code:
+/// Component
+import ProtectedRoute from './components/ProtectedRoute'
+
+/// All Pages
 import About from './pages/About'
 import Navbar from './components/Navbar'
 import LoginOrRegisterPage from './pages/LoginOrRegisterPage'
+import MyListsPage from './pages/MyListsPage'
 
 function App() {
 
@@ -15,6 +23,11 @@ function App() {
         <Route path = "/" element = {<div>Home Page</div>} />
         <Route path = "/about" element = {<About />} />
         <Route path = "/login" element = {<LoginOrRegisterPage />} />
+        <Route path = "/my-lists" element = {
+          <ProtectedRoute>
+              <MyListsPage />
+          </ProtectedRoute>
+        } />
       </Routes>
     </>
   )
---
+++ frontend/src/pages/MyListsPage.tsx
@@ -0,0 +1,8 @@
+export default function MyListsPage() {
+    return (
+        <div>
+            <h1>My Lists</h1>
+            <p>TODO: To Implement</p>
+        </div>
+    )
+}
